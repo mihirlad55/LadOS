2b26f8b6ca258229b821fcbaa4daa926ad3b155c
Mihir Lad <mihirlad55@gmail.com>
2020-05-05 19:50:11 -0400

    Add crontab feature

---
+++ required-features/7-crontab/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+BASE_DIR="$(dirname "$0")"
+
+echo "Installing cronie..."
+sudo pacman -S cronie --needed
+
+echo "Installing root crontab..."
+sudo crontab $BASE_DIR/root-cron
+
+echo "Enabling cronie..."
+sudo systemctl enable --now cronie
+
+echo "Done"
