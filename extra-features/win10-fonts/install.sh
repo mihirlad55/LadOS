9603c74f2f98ca20ed8a70fbbef2e5597d6412a6
Mihir Lad <mihirlad55@gmail.com>
2020-05-04 22:21:54 -0400

    Use URL to download win10 fonts

--- extra-features/win10-fonts/install.sh
+++ extra-features/win10-fonts/install.sh
@@ -7,6 +7,17 @@ REPO_PATH="$HOME/.cache/yay"
 mkdir -p "$REPO_PATH"
 
 git clone https://aur.archlinux.org/ttf-ms-win10.git $REPO_PATH/ttf-ms-win10
-cp -r $BASE_DIR/win-fonts/* $HOME/.cache/yay/ttf-ms-win10/
+echo "Enter url to windows 10 fonts zip file if available, otherwise leave blank"
+read url
+
+if [[ "$url" != "" ]]; then
+    curl $url --output /tmp/win10-fonts.zip
+    unzip -o /tmp/win10-fonts.zip -d $REPO_PATH/ttf-ms-win10
+else
+    echo "No url provided"
+    echo "Copy windows 10 fonts into $REPO_PATH/ttf-ms-win10/"
+    read -p "Press enter to continue..."
+fi
+
 
 yay -S --mflags --skipinteg ttf-ms-win10 
