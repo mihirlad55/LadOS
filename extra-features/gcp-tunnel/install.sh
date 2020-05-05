f88fdad8e3c001d57a726c1fe6292b980f66a84d
Mihir Lad <mihirlad55@gmail.com>
2020-05-04 21:28:05 -0400

    Put env-vars into seprarate file in gcp-tunnel

--- extra-features/gcp-tunnel/install.sh
+++ extra-features/gcp-tunnel/install.sh
@@ -4,24 +4,32 @@ BASE_DIR=$(dirname "$0")
 
 port=$(egrep /etc/ssh/sshd_config -e "^Port [0-9]*$")
 echo -n "Enter a port to run sshd on (blank to leave default: $port): "
-read port
+read new_port
 
-if [[ "$port" != "" ]]; then
+if [[ "$new_port" != "" ]]; then
+    port="$new_port"
     sudo sed -i /etc/ssh/sshd_config -e "s/^Port [0-9]*$/Port $port/"
-    sed -i $BASE_DIR/gcp-tunnel.service \
-        -e "s/^Environment=LOCAL_PORT=[0-9]*$/Environment=LOCAL_PORT=$port/"
 fi
 
-echo "Opening service file for any environment variable updates..."
+sed -i $BASE_DIR/gcp-tunnel.env \
+    -e "s/^LOCAL_PORT=[0-9]*$/LOCAL_PORT=$port/"
+
+echo "Opening environment file for updates..."
 read -p "Press enter to continue..."
 
 if [[ "$EDITOR" != "" ]]; then
-    $EDITOR $BASE_DIR/gcp-tunnel.service
+    $EDITOR $BASE_DIR/gcp-tunnel.env
 else
-    vi $BASE_DIR/gcp-tunnel.service
+    vi $BASE_DIR/gcp-tunnel.env
+fi
+
+if ! sudo test -e "/root/.ssh/id_rsa"; then
+    echo "Root's SSH keys are not setup"
+    sudo ssh-keygen
 fi
 
 sudo install -Dm 644 $BASE_DIR/gcp-tunnel.service /etc/systemd/system/gcp-tunnel.service
+sudo install -Dm 644 $BASE_DIR/gcp-tunnel.env /etc/gcp-tunnel.env
 
 sudo systemctl enable gcp-tunnel
 sudo systemctl start gcp-tunnel
