55e93525d645e02822dba3759897e3d0a4f74768
Mihir Lad <mihirlad55@gmail.com>
2020-05-04 21:29:15 -0400

    Add redshift feature

---
+++ extra-features/redshift/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+sudo pacman -S redshift geoclue2 --needed
+
+geoclue_conf_path="/etc/geoclue/geoclue.conf"
+
+# Allow redshift to use geoclue
+echo "Adding the following to $geoclue_conf_path..."
+echo "[redshift]" | sudo tee -a $geoclue_conf_path
+echo "allowed=true" | sudo tee -a $geoclue_conf_path
+echo "system=false" | sudo tee -a $geoclue_conf_path
+echo "users=" | sudo tee -a $geoclue_conf_path
+echo "url=https://location.services.mozilla.com/v1/geolocate?key=geoclue" |
+    sudo tee -a $geoclue_conf_path
+
+echo "Done installing redshift"
