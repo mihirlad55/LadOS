4120dcb031a41c4aa533ade433de2a0915572813
Mihir Lad <mihirlad55@gmail.com>
2020-05-06 17:13:19 -0400

    Edit logind.conf when installing user services

--- extra-features/user-services/install.sh
+++ extra-features/user-services/install.sh
@@ -7,6 +7,7 @@ SERVICE_PATH="$BASE_DIR/services"
 
 mkdir -p $INSTALL_PATH
 
+echo "Copying service files..."
 install -Dm 644 $SERVICE_PATH/battery-check-notify.service $INSTALL_PATH/battery-check-notify.service
 install -Dm 644 $SERVICE_PATH/compton.service $INSTALL_PATH/compton.service
 install -Dm 644 $SERVICE_PATH/dunst.service $INSTALL_PATH/dunst.service
@@ -21,6 +22,7 @@ install -Dm 644 $SERVICE_PATH/xautolock.service $INSTALL_PATH/xautolock.service
 install -Dm 644 $SERVICE_PATH/xbindkeys.service $INSTALL_PATH/xbindkeys.service
 install -Dm 644 $SERVICE_PATH/startup.service $INSTALL_PATH/startup.service
 
+echo "Enabling services..."
 systemctl --user enable battery-check-notify.service
 systemctl --user enable compton.service
 systemctl --user enable dunst.service
@@ -36,3 +38,8 @@ systemctl --user enable xautolock.service
 systemctl --user enable xbindkeys.service
 systemctl --user enable startup.service
 systemctl --user enable insync.service
+
+echo "Editing logind.conf to kill user processes on logout..."
+sudo sed -i /etc/systemd/logind.conf -e "s/^KillUserProcesses=.*$/KillUserProcesses=yes/"
+
+echo "Done"
