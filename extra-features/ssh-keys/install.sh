4e2e1f8527e8a5733ae3fcfdff388c7814fa0448
Mihir Lad <mihirlad55@gmail.com>
2020-05-08 17:11:44 -0400

    Fix ssh-keys install file

--- extra-features/ssh-keys/install.sh
+++ extra-features/ssh-keys/install.sh
@@ -1,31 +1,33 @@
 #!/usr/bin/bash
 
 BASE_DIR="$(dirname "$0")"
-DEFAULT_USER_SSH_DIR="$BASE_DIR/default-user/.ssh"
+USER_SSH_DIR="$BASE_DIR/user/.ssh"
 ROOT_SSH_DIR="$BASE_DIR/root/.ssh"
 
-if [[ -d "$BASE_DIR/default-user/.ssh" ]] &&
-    [[ -d "$BASE_DIR/root/.ssh"]]; then
+if [[ -d "$BASE_DIR/user/.ssh" ]] &&
+    [[ -d "$BASE_DIR/root/.ssh" ]]; then
     
-    echo "Copying default-user ssh files to $HOME/.ssh..."
+    echo "Copying user ssh files to $HOME/.ssh..."
     mkdir -p "$HOME/.ssh"
     chmod 700 "$HOME/.ssh"
-    install -Dm 600 "$DEFAULT_USER_SSH_DIR/id_rsa" "$HOME/.ssh/id_rsa"
-    install -Dm 600 "$DEFAULT_USER_SSH_DIR/config" "$HOME/.ssh/config"
-    install -Dm 600 "$DEFAULT_USER_SSH_DIR/authorized_keys" "$HOME/.ssh/authorized_keys"
-    install -Dm 644 "$DEFAULT_USER_SSH_DIR/id_rsa.pub" "$HOME/.ssh/id_rsa.pub"
-    install -Dm 644 "$DEFAULT_USER_SSH_DIR/known_hosts" "$HOME/.ssh/known_hosts"
+    install -Dm 600 "$USER_SSH_DIR/id_rsa" "$HOME/.ssh/id_rsa"
+    install -Dm 600 "$USER_SSH_DIR/config" "$HOME/.ssh/config"
+    install -Dm 600 "$USER_SSH_DIR/authorized_keys" "$HOME/.ssh/authorized_keys"
+    install -Dm 644 "$USER_SSH_DIR/id_rsa.pub" "$HOME/.ssh/id_rsa.pub"
+    install -Dm 644 "$USER_SSH_DIR/known_hosts" "$HOME/.ssh/known_hosts"
+    chown -R $USER "$HOME/.ssh"
 
     echo "Copying root ssh files to /root/.ssh"
-    mkdir -p "/root/.ssh"
-    chmod 700 "/root/.ssh"
-    install -Dm 600 "$ROOT_SSH_DIR/id_rsa" "/root/.ssh/id_rsa"
-    install -Dm 600 "$ROOT_SSH_DIR/config" "/root/.ssh/config"
-    install -Dm 600 "$ROOT_SSH_DIR/authorized_keys" "/root/.ssh/authorized_keys"
-    install -Dm 644 "$ROOT_SSH_DIR/id_rsa.pub" "/root/.ssh/id_rsa.pub"
-    install -Dm 644 "$ROOT_SSH_DIR/known_hosts" "/root/.ssh/known_hosts"
+    sudo mkdir -p "/root/.ssh"
+    sudo chmod 700 "/root/.ssh"
+    sudo install -Dm 600 "$ROOT_SSH_DIR/id_rsa" "/root/.ssh/id_rsa"
+    sudo install -Dm 600 "$ROOT_SSH_DIR/config" "/root/.ssh/config"
+    sudo install -Dm 600 "$ROOT_SSH_DIR/authorized_keys" "/root/.ssh/authorized_keys"
+    sudo install -Dm 644 "$ROOT_SSH_DIR/id_rsa.pub" "/root/.ssh/id_rsa.pub"
+    sudo install -Dm 644 "$ROOT_SSH_DIR/known_hosts" "/root/.ssh/known_hosts"
+    sudo chown -R root "/root/.ssh"
 
     echo "Done copying ssh files"
 else
-    echo "Root or default-user .ssh folder missing"
+    echo "Root or user .ssh folder missing"
 fi
