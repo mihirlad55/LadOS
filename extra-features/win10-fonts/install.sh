522ff45458e8416b9e6867efcd33c304312c6413
Mihir Lad <mihirlad55@gmail.com>
2020-05-08 20:27:21 -0400

    Copy existing win10-fonts if present

--- extra-features/win10-fonts/install.sh
+++ extra-features/win10-fonts/install.sh
@@ -7,17 +7,22 @@ REPO_PATH="$HOME/.cache/yay"
 mkdir -p "$REPO_PATH"
 
 git clone https://aur.archlinux.org/ttf-ms-win10.git $REPO_PATH/ttf-ms-win10
-echo "Enter url to windows 10 fonts zip file if available, otherwise leave blank"
-read url
 
-if [[ "$url" != "" ]]; then
-    curl $url --output /tmp/win10-fonts.zip
-    unzip -o /tmp/win10-fonts.zip -d $REPO_PATH/ttf-ms-win10
+if [[ -d "$BASE_DIR/win10-fonts" ]] && [[ "$(ls $BASE_DIR/win10-fonts)" != "" ]]; then
+    echo "Found win10-fonts"
+    echo "Copying fonts to $REPO_PATH/ttf-ms-win10/"
+    cp -rf $BASE_DIR/win10-fonts/* $REPO_PATH/ttf-ms-win10/
 else
-    echo "No url provided"
-    echo "Copy windows 10 fonts into $REPO_PATH/ttf-ms-win10/"
-    read -p "Press enter to continue..."
-fi
+    echo "Enter url to windows 10 fonts zip file if available, otherwise leave blank"
+    read url
 
+    if [[ "$url" != "" ]]; then
+        curl $url --output /tmp/win10-fonts.zip
+        unzip -o /tmp/win10-fonts.zip -d $REPO_PATH/ttf-ms-win10
+    else
+        echo "No url provided"
+        exit 1
+    fi
+fi
 
 yay -S --mflags --skipinteg --needed --noconfirm ttf-ms-win10
