b3f83bb2e371b2667106baa91701d2216bc19601
Mihir Lad <mihirlad55@gmail.com>
2020-05-08 20:21:38 -0400

    Update webkit2 feature

--- extra-features/webkit2/install.sh
+++ extra-features/webkit2/install.sh
@@ -2,17 +2,29 @@
 
 BASE_DIR="$(dirname "$0")"
 
-sudo pacman -S lightdm-webkit2-greeter --needed --noconfirm
-yay -S lightdm-webkit2-theme-material2 --needed --noconfirm
+sudo pacman -S lightdm-webkit2-greeter accountsservice --needed --noconfirm
 
 echo "Changing greeter session in /etc/lightdm/lightdm.conf"
 sudo sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/' /etc/lightdm/lightdm.conf
 
-echo "To change greeter avatar, copy png to /var/lib/AccountsService/<username>."
-echo "Note: <username> is the png itself, not a folder"
+if [[ -f "$BASE_DIR/$USER.png" ]]; then
+    sudo install -Dm 644 "$BASE_DIR/$USER.png" /var/lib/AccountsService/icons/$USER.png
+else
+    echo "To change greeter avatar, copy png to /var/lib/AccountsService/icons/$USER.png"
+fi
 
-echo "To add backgrounds, copy backgrounds to /usr/share/backgrounds"
-echo "Make sure the avatar and background are readable by everyone"
+echo "Creating /var/lib/AccountsService/user/$USER ini"
+echo "[User]" | sudo tee /var/lib/AccountsService/users/$USER > /dev/null
+echo "Icon=/var/lib/AccountsService/icons/$USER.png" | 
+    sudo tee -a /var/lib/AccountsService/users/$USER > /dev/null
 
-echo "Copying lightdm-webkit2-greeter.conf to /etc/lightdm"
-sudo install -Dm 644 $BASE_DIR/lightdm-webkit2-greeter.conf /etc/lightdm/lightdm-webkit2-greeter.conf
+if [[ -d "$BASE_DIR/backgrounds" ]]; then
+    echo "Copying backgrounds from $BASE_DIR/backgrounds to /usr/share/backgrounds/"
+    sudo install -m 644 $BASE_DIR/backgrounds/* /usr/share/backgrounds/
+    echo "You will have to set the background from the login screen"
+else
+    echo "To add backgrounds, copy backgrounds to /usr/share/backgrounds"
+    echo "Make sure the avatar and background are readable by everyone"
+fi
+
+echo "Done installing lightdm-webkit2-greeter"
