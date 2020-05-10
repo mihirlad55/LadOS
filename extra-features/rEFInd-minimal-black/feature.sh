545a2c430398e93e6dc734efef50b13d63e10ad9
Mihir Lad <mihirlad55@gmail.com>
2020-05-10 00:57:53 -0400

    Update rEFInd-minimal-black to follow template

---
+++ extra-features/rEFInd-minimal-black/feature.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/bash
+
+# Get absolute path to directory of script
+BASE_DIR="$( readlink -f "$(dirname "$0")" )"
+# Get absolute path to root of repo
+LAD_OS_DIR="$( echo $BASE_DIR | grep -o ".*/LadOS/" | sed 's/.$//')"
+
+feature_name="rEFInd-minimal-black"
+feature_desc="Install and setup rEFInd with minimal-black theme"
+
+provides=()
+new_files=("/boot/EFI/refind/themes" \
+    "/boot/EFI/refind/refind.conf" \
+    "/boot/EFI/refind/refind-manual.conf" \
+    "/boot/EFI/refind/refind-options.conf")
+modified_files=()
+temp_files=("/tmp/refind-options.conf" \
+    "/tmp/refind-manual.conf")
+
+depends_aur=()
+depends_pacman=(refind)
+
+
+function check_install() {
+    HEAD="$(cat /boot/EFI/refind/themes/rEFInd-minimal-black/.git/HEAD)"
+    if grep "include refind-manual.conf" /boot/EFI/refind/refind.conf > /dev/null &&
+        [[ -e "/boot/EFI/refind/refind-options.conf" ]] &&
+        [[ -e "/boot/EFI/refind/refind-manual.conf" ]] &&
+        [[ -d "/boot/EFI/refind/themes/rEFInd-minimal-black" ]]; then
+        echo "$feature_name is installed"
+        return 0
+    else
+        echo "$feature_name is not installed"
+        return 1
+    fi
+}
+
+function prepare() {
+    cp $BASE_DIR/refind-options.conf /tmp/refind-options.conf
+    cp $BASE_DIR/refind-manual.conf /tmp/refind-manual.conf
+}
+
+function install() {
+    shopt -s expand_aliases
+    ( [[ "$USER" = "root" ]] || ! command -v sudo &> /dev/null ) && alias sudo=
+
+    sudo refind-install
+
+    sudo mkdir -p /boot/EFI/refind/themes
+    sudo rm -rf /boot/EFI/refind/themes/rEFInd-minimal-black
+    sudo git clone https://github.com/andersfischernielsen/rEFInd-minimal-black.git /boot/EFI/refind/themes/rEFInd-minimal-black
+
+    swap_path=$(cat /etc/fstab | grep -P -B 1 \
+        -e "UUID=[a-zA-Z0-9\-]*[\t ]+none[\t ]+swap" | head -n1 | sed 's/# *//')
+    root_path=$(cat /etc/fstab | grep -P -B 1 \
+        -e "UUID=[a-zA-Z0-9\-]*[\t ]+/[\t ]+" | head -n1 | sed 's/# *//')
+
+    partuuid=$(blkid -s PARTUUID -o value $root_path)
+
+    sed -i /tmp/refind-options.conf -e "s/root=PARTUUID=[a-z0-9\-]*/root=PARTUUID=$partuuid/"
+    sed -i /tmp/refind-options.conf -e "s;resume=;resume=$swap_path;"
+
+    echo "Opening configuration files for any changes. The root PARTUUID has already been set along with the swap paritition path for resume"
+    read -p "Press enter to continue..."
+
+    if [[ "$EDITOR" != "" ]]; then
+        $EDITOR /tmp/refind-options.conf
+        $EDITOR /tmp/refind-manual.conf
+    else
+        vim /tmp/refind-options.conf
+        vim /tmp/refind-manual.conf
+    fi
+
+    echo "Copying configuration files to /boot/EFI/refind/..."
+    sudo install -Dm 755 /tmp/refind-options.conf /boot/EFI/refind/refind-options.conf
+    sudo install -Dm 755 /tmp/refind-manual.conf /boot/EFI/refind/refind-manual.conf
+    sudo install -Dm 755 /usr/share/refind/refind.conf-sample /boot/EFI/refind/refind.conf
+
+    echo "
+    include refind-manual.conf
+    include refind-options.conf
+    include themes/rEFInd-minimal-black/theme.conf
+    " | sudo tee -a /boot/EFI/refind/refind.conf
+
+    echo "Done"
+}
+
+function cleanup() {
+    echo "Removing ${temp_files[@]}..."
+    rm -f /tmp/refind-options.conf
+    rm -f /tmp/refind-manual.conf
+}
+
+source "$LAD_OS_DIR/common/feature_common.sh"
