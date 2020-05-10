89c2d9697f1bffd0f1408ebf9c4b3d89c22b8b53
Mihir Lad <mihirlad55@gmail.com>
2020-05-09 21:07:02 -0400

    Update 5-dwm to follow feature template

---
+++ required-features/5-dwm/feature.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/bash
+
+# Get absolute path to directory of script
+BASE_DIR="$( readlink -f "$(dirname "$0")" )"
+# Get absolute path to root of repo
+LAD_OS_DIR="$( echo $BASE_DIR | grep -o ".*/LadOS/" | sed 's/.$//')"
+
+feature_name="DWM"
+feature_desc="Install DWM (Dynamic Window Manager)"
+
+provides=()
+new_files=("/usr/local/bin/dwm" \
+            "/usr/local/bin/startdwm" \
+            "/usr/share/xsessions/dwm.desktop" \
+            "/usr/local/share/man/man1/dwm.1")
+modified_files=()
+temp_files=("/tmp/dwm")
+
+depends_aur=()
+depends_pacman=(lightdm lightdm-gtk-greeter xorg-server xorg-server-common)
+
+
+function check_install() {
+    for f in ${new_files[@]}; do
+        if [[ ! -f "$f" ]]; then
+            echo "$f is missing"
+            echo "$feature_name is not installed"
+            return 1
+        fi
+    done
+
+    echo "$feature_name is installed"
+    return 0
+}
+
+function prepare() {
+    git clone https://github.com/mihirlad55/dwm /tmp/dwm
+}
+
+function install() {
+    (cd /tmp/dwm && sudo make clean install)
+}
+
+function post_install() {
+    sudo systemctl enable lightdm
+
+    sudo systemctl set-default graphical.target
+}
+
+function cleanup() {
+    rm -rf /tmp/dwm
+}
+
+source "$LAD_OS_DIR/common/feature_common.sh"
+
+
