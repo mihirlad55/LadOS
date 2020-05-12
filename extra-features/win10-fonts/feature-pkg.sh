2e3da7a3c35a3af4032fd4c77cbf5cae5ec26dac
Mihir Lad <mihirlad55@gmail.com>
2020-05-11 23:35:18 -0400

    Split win10-fonts feature into two feature scripts
    The feature script that is loaded depends on if the ttf-ms-win10 package exists
    in cache or under localrepo already. If it does not exist pre-compiled, it is
    made from scratch.

---
+++ extra-features/win10-fonts/feature-pkg.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+# Get absolute path to directory of script
+BASE_DIR="$( readlink -f "$(dirname "$0")" )"
+# Get absolute path to root of repo
+LAD_OS_DIR="$( echo $BASE_DIR | grep -o ".*/LadOS/" | sed 's/.$//')"
+
+feature_name="win10-fonts"
+feature_desc="Install windows 10 fonts (using localrepo)"
+
+provides=(ttf-ms-win10)
+new_files=()
+modified_files=()
+temp_files=()
+
+depends_aur=()
+depends_pacman=()
+depends_pip3=()
+
+
+function check_install() {
+    if pacman -Q ttf-ms-win10 > /dev/null; then
+        echo "$feature_name is installed"
+        return 0
+    else
+        echo "$feature_name is not installed"
+        return 1
+    fi
+
+}
+
+function install() {
+    sudo pacman -S $provides --noconfirm --needed
+}
+
+
+source "$LAD_OS_DIR/common/feature_common.sh"
