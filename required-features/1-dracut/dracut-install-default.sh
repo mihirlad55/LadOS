bcba105adb0a1a185741cace2601f174df368d4f
Mihir Lad <mihirlad55@gmail.com>
2020-05-18 21:33:53 -0400

    Fix dracut-install-default.sh

--- required-features/1-dracut/dracut-install-default.sh
+++ required-features/1-dracut/dracut-install-default.sh
@@ -5,6 +5,6 @@ args=('--force' '--uefi')
 kver="$(uname -r)"
 read -r pkgbase < /usr/lib/modules/$kver/pkgbase
 
-install -Dm0644 "/${line%'/pkgbase'}/vmlinuz" "/boot/vmlinuz-${pkgbase}"
+install -Dm0644 "/usr/lib/modules/${kver}/vmlinuz" "/boot/vmlinuz-${pkgbase}"
 dracut "${args[@]}" --hostonly "/boot/${pkgbase}.efi" --kver "$kver"
 dracut "${args[@]}" --no-hostonly "/boot/${pkgbase}-fallback.efi" --kver "$kver"
