b0e77868cc4dfe9ec650c397a56eb2f626685c7e
Mihir Lad <mihirlad55@gmail.com>
2020-05-26 22:29:22 -0400

    Shellcheck crontab feature

--- required-features/8-crontab/feature.sh
+++ required-features/8-crontab/feature.sh
@@ -4,7 +4,7 @@
 # Get absolute path to directory of script
 BASE_DIR="$( readlink -f "$(dirname "$0")" )"
 # Get absolute path to root of repo
-LAD_OS_DIR="$( echo $BASE_DIR | grep -o ".*/LadOS/" | sed 's/.$//')"
+LAD_OS_DIR="$( echo "$BASE_DIR" | grep -o ".*/LadOS/" | sed 's/.$//')"
 
 source "$LAD_OS_DIR/common/feature_header.sh"
 
@@ -21,8 +21,8 @@ depends_pacman=("cronie")
 
 
 function check_install() {
-    echo "Checking if root crontab matches $(cat $BASE_DIR/root-cron)"
-    if sudo diff /var/spool/cron/root $BASE_DIR/root-cron; then
+    qecho "Checking if root crontab matches $(cat "$BASE_DIR/root-cron")"
+    if sudo diff /var/spool/cron/root "$BASE_DIR/root-cron"; then
         qecho "$feature_name is installed"
     else
         echo "$feature_name is not installed" >&2
@@ -31,13 +31,13 @@ function check_install() {
 
 function install() {
     qecho "Installing root crontab..."
-    cat $BASE_DIR/root-cron
-    sudo crontab $BASE_DIR/root-cron
+    if [[ -n "$VERBOSITY" ]]; then cat "$BASE_DIR/root-cron"; fi
+    sudo crontab "$BASE_DIR/root-cron"
 }
 
 function post_install() {
     qecho "Enabling cronie..."
-    sudo systemctl enable -f ${SYSTEMD_FLAGS[*]} cronie
+    sudo systemctl enable "${SYSTEMD_FLAGS[@]}" cronie
 }
 
 
