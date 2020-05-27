52779447b777b4c571f27d83bf7aceee16fa9d29
Mihir Lad <mihirlad55@gmail.com>
2020-05-27 16:47:42 -0400

    Shellcheck plymouth feature

--- optional-features/plymouth/feature.sh
+++ optional-features/plymouth/feature.sh
@@ -3,7 +3,7 @@
 # Get absolute path to directory of script
 BASE_DIR="$( readlink -f "$(dirname "$0")" )"
 # Get absolute path to root of repo
-LAD_OS_DIR="$( echo $BASE_DIR | grep -o ".*/LadOS/" | sed 's/.$//')"
+LAD_OS_DIR="$( echo "$BASE_DIR" | grep -o ".*/LadOS/" | sed 's/.$//' )"
 DRACUT_CONF_DIR="/etc/dracut.conf.d"
 CMDLINE_DIR="/etc/cmdline.d"
 CMDLINE_FILE="$CMDLINE_DIR/plymouth.conf"
@@ -29,10 +29,10 @@ depends_pacman=()
 
 
 function check_install() {
-    if diff $BASE_DIR/deus_ex /usr/share/plymouth/themes/deus_ex &&
-        diff $BASE_DIR/plymouthd.conf /etc/plymouth/plymouthd.conf &&
-        diff $BASE_DIR/plymouth-dracut.conf $DRACUT_CONF_DIR/plymouth-dracut.conf &&
-        diff $BASE_DIR/plymouth-cmdline.conf $CMDLINE_FILE; then
+    if diff "$BASE_DIR/deus_ex" /usr/share/plymouth/themes/deus_ex &&
+        diff "$BASE_DIR/plymouthd.conf" /etc/plymouth/plymouthd.conf &&
+        diff "$BASE_DIR/plymouth-dracut.conf" "$DRACUT_CONF_DIR/plymouth-dracut.conf" &&
+        diff "$BASE_DIR/plymouth-cmdline.conf" "$CMDLINE_FILE"; then
         qecho "$feature_name is installed"
         return 0
     else
@@ -43,10 +43,10 @@ function check_install() {
 
 function install() {
     qecho "Copying theme..."
-    sudo cp -r $BASE_DIR/deus_ex /usr/share/plymouth/themes/
+    sudo cp -rft /usr/share/plymouth/themes "$BASE_DIR/deus_ex"
 
     qecho "Copying plymouth.d..."
-    sudo install -Dm 644 $BASE_DIR/plymouthd.conf /etc/plymouth/plymouthd.conf
+    sudo install -Dm 644 "$BASE_DIR/plymouthd.conf" /etc/plymouth/plymouthd.conf
 
     qecho "Copying plymouth-dracut.conf to $DRACUT_CONF_DIR..."
     sudo install -Dm 644 "$BASE_DIR/plymouth-dracut.conf" "$DRACUT_CONF_DIR/plymouth-dracut.conf"    
@@ -60,20 +60,23 @@ function install() {
 
 function post_install() {
     qecho "Disabling lightdm.service..."
-    sudo systemctl disable -f $VERBOSITY_FLAG lightdm
+    sudo systemctl disable "${SYSTEMD_FLAGS[@]}" lightdm
     qecho "Enabling lightdm-plymouth.service"
-    sudo systemctl enable -f ${SYSTEMD_FLAGS[*]} lightdm-plymouth
+    sudo systemctl enable "${SYSTEMD_FLAGS[@]}" lightdm-plymouth
 }
 
 function uninstall() {
     qecho "Disbaling lightdm-plymouth.service"
-    sudo systemctl disable -f ${SYSTEMD_FLAGS[*]} lightdm-plymouth
+    sudo systemctl disable "${SYSTEMD_FLAGS[@]}" lightdm-plymouth
 
     qecho "Enabling lightdm.service..."
-    sudo systemctl enable -f $VERBOSITY_FLAG lightdm
+    sudo systemctl enable "${SYSTEMD_FLAGS[@]}" lightdm
 
-    qecho "Removing ${new_files[@]}..."
-    rm -f "${new_files[@]} /etc/plymouth/plymouthd.conf"
+    qecho "Removing ${new_files[*]}..."
+    rm -f "${new_files[@]}"
+
+    qecho "Removing /etc/plymouth/plymouthd.conf..."
+    rm -f "/etc/plymouth/plymouthd.conf"
 
     qecho "Updating image..."
     sudo /usr/local/bin/dracut-install-default.sh
