f1bc19b63102d2fa5e7902c6a01113b2a45d72fd
Mihir Lad <mihirlad55@gmail.com>
2020-05-27 15:35:50 -0400

    Shellcheck doom emacs feature

--- optional-features/doom-emacs/feature.sh
+++ optional-features/doom-emacs/feature.sh
@@ -4,7 +4,7 @@
 # Get absolute path to directory of script
 BASE_DIR="$( readlink -f "$(dirname "$0")" )"
 # Get absolute path to root of repo
-LAD_OS_DIR="$( echo $BASE_DIR | grep -o ".*/LadOS/" | sed 's/.$//')"
+LAD_OS_DIR="$( echo "$BASE_DIR" | grep -o ".*/LadOS/" | sed 's/.$//')"
  
 source "$LAD_OS_DIR/common/feature_header.sh"
 
@@ -19,10 +19,11 @@ temp_files=("/tmp/doom-emacs")
 depends_aur=()
 depends_pacman=(emacs)
 
+DOOM_EMACS_URL="https://github.com/hlissner/doom-emacs"
 
 
 function check_install() {
-    if $HOME/.emacs.d/bin/doom sync; then
+    if "$HOME/.emacs.d/bin/doom" sync; then
         qecho "$feature_name is installed"
         return 0
     else
@@ -35,26 +36,26 @@ function install() {
     qecho "Cloning doom emacs..."
 
     if [[ ! -d "/tmp/doom-emacs" ]]; then
-        git clone --depth 1 $VERBOSITY_FLAG https://github.com/hlissner/doom-emacs "/tmp/doom-emacs"
+        git clone --depth 1 "${V_FLAG[@]}" "$DOOM_EMACS_URL" "/tmp/doom-emacs"
     fi
-    (shopt -s dotglob && cp -rf /tmp/doom-emacs/* $HOME/.emacs.d)
+    (shopt -s dotglob && cp -rf /tmp/doom-emacs/* "$HOME/.emacs.d")
 
     qecho "Installing doom emacs"
-    $HOME/.emacs.d/bin/doom -y install
+    "$HOME/.emacs.d/bin/doom" -y install
 
     qecho "Syncing doom emacs"
-    $HOME/.emacs.d/bin/doom -y sync
+    "$HOME/.emacs.d/bin/doom" -y sync
 
     qecho "Done installing doom emacs"
 }
 
 function cleanup() {
-    qecho "Removing ${temp_files[@]}..."
+    qecho "Removing ${temp_files[*]}..."
     rm -rf "${temp_files[@]}"
 }
 
 function uninstall() {
-    qecho "Removing ${new_files[@]}..."
+    qecho "Removing ${new_files[*]}..."
     rm -f "${new_files[@]}"
 }
 
