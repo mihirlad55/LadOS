1b89a76c6362810ecf66fa02ff7af3069f9852bf
Mihir Lad <mihirlad55@gmail.com>
2020-05-27 16:52:42 -0400

    Shellcheck power-desktop-options feature
    Also remove terminal directive from desktop files since it is not necessary to
    run these commands in a terminal.

--- optional-features/power-desktop-options/feature.sh
+++ optional-features/power-desktop-options/feature.sh
@@ -4,7 +4,7 @@
 # Get absolute path to directory of script
 BASE_DIR="$( readlink -f "$(dirname "$0")" )"
 # Get absolute path to root of repo
-LAD_OS_DIR="$( echo $BASE_DIR | grep -o ".*/LadOS/" | sed 's/.$//')"
+LAD_OS_DIR="$( echo "$BASE_DIR" | grep -o ".*/LadOS/" | sed 's/.$//' )"
 DESKTOP_FILES_DIR="$BASE_DIR/power-desktop-files/"
 INSTALL_DIR="/usr/share/applications"
 
@@ -14,11 +14,13 @@ feature_name="power-desktop-files"
 feature_desc="Install lock, hibernate, logout, reboot, and poweroff desktop files"
 
 provides=()
-new_files=("$INSTALL_DIR/hibernate.desktop" \
+new_files=( \
+    "$INSTALL_DIR/hibernate.desktop" \
     "$INSTALL_DIR/lock.desktop" \
     "$INSTALL_DIR/logout.desktop" \
     "$INSTALL_DIR/reboot.desktop" \
-    "$INSTALL_DIR/poweroff.desktop")
+    "$INSTALL_DIR/poweroff.desktop" \
+)
 modified_files=()
 temp_files=()
 
@@ -42,20 +44,20 @@ function check_install() {
 
 function install() {
     qecho "Copying files..."
-    sudo install -Dm 644 $DESKTOP_FILES_DIR/hibernate.desktop \
-        $INSTALL_DIR/hibernate.desktop
-    sudo install -Dm 644 $DESKTOP_FILES_DIR/lock.desktop \
-        $INSTALL_DIR/lock.desktop
-    sudo install -Dm 644 $DESKTOP_FILES_DIR/logout.desktop \
-        $INSTALL_DIR/logout.desktop
-    sudo install -Dm 644 $DESKTOP_FILES_DIR/reboot.desktop \
+    sudo install -Dm 644 "$DESKTOP_FILES_DIR/hibernate.desktop" \
+        "$INSTALL_DIR/hibernate.desktop"
+    sudo install -Dm 644 "$DESKTOP_FILES_DIR/lock.desktop" \
+        "$INSTALL_DIR/lock.desktop"
+    sudo install -Dm 644 "$DESKTOP_FILES_DIR/logout.desktop" \
+        "$INSTALL_DIR/logout.desktop"
+    sudo install -Dm 644 "$DESKTOP_FILES_DIR/reboot.desktop" \
         $INSTALL_DIR/reboot.desktop
-    sudo install -Dm 644 $DESKTOP_FILES_DIR/poweroff.desktop \
-        $INSTALL_DIR/poweroff.desktop
+    sudo install -Dm 644 "$DESKTOP_FILES_DIR/poweroff.desktop" \
+        "$INSTALL_DIR/poweroff.desktop"
 }
 
 function uninstall() {
-    qecho "Removing ${new_files[@]}..."
+    qecho "Removing ${new_files[*]}..."
     rm -f "${new_files[@]}"
 }
 
