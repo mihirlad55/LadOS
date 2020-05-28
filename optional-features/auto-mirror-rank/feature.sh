d6b4f1982fe44dc5adafa58beaf751a07d7c407c
Mihir Lad <mihirlad55@gmail.com>
2020-05-28 15:43:20 -0400

    Improve code style for auto-mirror-rank feature
    See issue #3

--- optional-features/auto-mirror-rank/feature.sh
+++ optional-features/auto-mirror-rank/feature.sh
@@ -1,62 +1,64 @@
 #!/usr/bin/bash
 
 # Get absolute path to directory of script
-BASE_DIR="$( readlink -f "$(dirname "$0")" )"
+readonly BASE_DIR="$( readlink -f "$(dirname "$0")" )"
 # Get absolute path to root of repo
-LAD_OS_DIR="$( echo "$BASE_DIR" | grep -o ".*/LadOS/" | sed 's/.$//' )"
+readonly LAD_OS_DIR="$( echo "$BASE_DIR" | grep -o ".*/LadOS/" | sed 's/.$//' )"
+readonly BASE_SERVICE="$BASE_DIR/auto-mirror-rank.service"
+readonly BASE_UPDATE_SH="$BASE_DIR/update-pacman-mirrors"
+readonly NEW_SERVICE="/etc/systemd/system/auto-mirror-rank.service"
+readonly NEW_UPDATE_SH="/usr/local/bin/update-pacman-mirrors"
 
 source "$LAD_OS_DIR/common/feature_header.sh"
 
-SERVICE_FILE="/etc/systemd/system/auto-mirror-rank.service"
-UPDATE_PACMAN_MIRRORS_SCRIPT="/usr/local/bin/update-pacman-mirrors"
-
-feature_name="Auto Mirror Rank"
-feature_desc="Rank pacman mirrors on startup"
-
-conflicts=()
-
-provides=()
-new_files=("$SERVICE_FILE" "$UPDATE_PACMAN_MIRRORS_FILE")
-modified_files=()
-temp_files=()
-
-depends_aur=()
-depends_pacman=(pacman-contrib)
-depends_pip3=()
+readonly FEATURE_NAME="Auto Mirror Rank"
+readonly FEATURE_DESC="Rank pacman mirrors on startup"
+readonly CONFLICTS=()
+readonly PROVIDES=()
+readonly NEW_FILES=( \
+    "$NEW_SERVICE" \
+    "$NEW_UPDATE_SH" \
+)
+readonly MODIFIED_FILES=()
+readonly TEMP_FILES=()
+readonly DEPENDS_AUR=()
+readonly DEPENDS_PACMAN=(pacman-contrib)
+readonly DEPENDS_PIP3=()
 
 
 
 function check_install() {
-    for f in "${new_files[@]}"; do
+    local f
+
+    for f in "${NEW_FILES[@]}"; do
         if [[ ! -f "$f" ]]; then
             echo "$f is missing" >&2
-            echo "$feature_name is not installed" >&2
+            echo "$FEATURE_NAME is not installed" >&2
             return 1
         fi
     done
 
-    qecho "$feature_name is installed"
+    qecho "$FEATURE_NAME is installed"
     return 0
 }
 
 function install() {
-    qecho "Copying update-pacman-mirrors to /usr/local/bin..."
-    sudo install -Dm 755 "$BASE_DIR/update-pacman-mirrors" "$UPDATE_PACMAN_MIRRORS_SCRIPT"
+    qecho "Copying $BASE_UPDATE_SH to $NEW_UPDATE_SH..."
+    sudo install -Dm 755 "$BASE_UPDATE_SH" "$NEW_UPDATE_SH"
 
-    qecho "Copying auto-mirror-rank.service to /etc/systemd/system..."
-    sudo install -Dm 644 "$BASE_DIR/auto-mirror-rank.service" "$SERVICE_FILE"
+    qecho "Copying $BASE_SERVICE to $NEW_SERVICE..."
+    sudo install -Dm 644 "$BASE_SERVICE" "$NEW_SERVICE"
 }
 
 function post_install() {
     qecho "Enabling auto-mirror-rank.service"
-    sudo systemctl enable "${SYSTEMD_FLAGS[@]}" "auto-mirror-rank.service"
+    sudo systemctl enable "${SYSTEMD_FLAGS[@]}" auto-mirror-rank.service
 }
 
 function uninstall() {
-    qecho "Removing ${new_files[*]}..."
-    sudo rm -f "${new_files[@]}"
+    qecho "Removing ${NEW_FILES[*]}..."
+    sudo rm -f "${NEW_FILES[@]}"
 }
 
-source "$LAD_OS_DIR/common/feature_footer.sh"
 
-# vim:ft=sh
+source "$LAD_OS_DIR/common/feature_footer.sh"
