7c6c302bc34b584f53ed482bc33903ba5678ffc7
Mihir Lad <mihirlad55@gmail.com>
2020-05-28 15:45:26 -0400

    Improve style for polybar-weather-module feature
    See issue #3.

--- optional-features/weather-polybar-module/feature.sh
+++ optional-features/weather-polybar-module/feature.sh
@@ -1,75 +1,88 @@
 #!/usr/bin/bash
 
-
 # Get absolute path to directory of script
-BASE_DIR="$( readlink -f "$(dirname "$0")" )"
+readonly BASE_DIR="$( readlink -f "$(dirname "$0")" )"
 # Get absolute path to root of repo
-LAD_OS_DIR="$( echo "$BASE_DIR" | grep -o ".*/LadOS/" | sed 's/.$//' )"
-CONF_DIR="$LAD_OS_DIR/conf/weather-polybar-module"
+readonly LAD_OS_DIR="$( echo "$BASE_DIR" | grep -o ".*/LadOS/" | sed 's/.$//' )"
+readonly CONF_DIR="$LAD_OS_DIR/conf/weather-polybar-module"
+readonly INSTALL_DIR="$HOME/.apikeys/openweathermap.key"
+readonly CONF_KEY_FILE="$CONF_DIR/openweathermap.key"
+readonly NEW_KEY_FILE="$INSTALL_DIR/openweathermap.key"
+readonly TMP_KEY_FILE="/tmp/openweathermap.key"
 
 source "$LAD_OS_DIR/common/feature_header.sh"
 
-KEY_PATH="$CONF_DIR/openweathermap.key"
-INSTALL_PATH="$HOME/.apikeys/openweathermap.key"
+readonly FEATURE_NAME="weather-polybar-module"
+readonly FEATURE_DESC="Install OpenWeatherMap API Key for polybar weather module"
+readonly PROVIDES=()
+readonly NEW_FILES=("$NEW_KEY_FILE")
+readonly MODIFIED_FILES=()
+readonly TEMP_FILES=("$TMP_KEY_FILE")
+readonly DEPENDS_AUR=()
+readonly DEPENDS_PACMAN=()
+readonly DEPENDS_PIP3=()
 
-feature_name="weather-polybar-module"
-feature_desc="Install OpenWeatherMap API Key for polybar weather module"
-
-provides=()
-new_files=("$HOME/.apikeys/openweathermap.key")
-modified_files=()
-temp_files=("/tmp/openweathermap.key")
-
-depends_aur=()
-depends_pacman=()
-depends_pip3=()
 
 
 function check_install() {
-    if [[ "$(cat "$HOME/.apikeys/openweathermap.key")" != "" ]]; then
-        qecho "$feature_name is installed"
-        return 0
-    else
-        echo "$feature_name is not installed" >&2
-        return 1
+    local key
+
+    if [[ -f "$NEW_KEY_FILE" ]]; then
+        key="$(cat "$NEW_KEY_FILE")"
+
+        if [[ "$key" != "" ]]; then
+            qecho "$FEATURE_NAME is installed"
+            return 0
+        fi
     fi
+
+    echo "$FEATURE_NAME is not installed" >&2
+    return 1
 }
 
 function check_conf() {
-    if [[ -e "$KEY_PATH" ]] && [[ "$(cat "$KEY_PATH")" != "" ]]; then
-        qecho "Configuration key found at $KEY_PATH"
-        return 0
-    else
-        echo "No configuration found or not in correct format at $KEY_PATH" >&2
-        return 1
+    local key
+    if [[ -f "$CONF_KEY_FILE" ]]; then
+        key="$(cat "$CONF_KEY_FILE")"
+
+        if [[ "$key" != "" ]]; then
+            qecho "Configuration key found at $CONF_KEY_FILE"
+            return 0
+        fi
     fi
+
+    echo "No configuration found or not in correct format at $CONF_KEY_FILE" >&2
+    return 1
 }
 
 function install() {
+    local key
+
     if ! check_conf; then
         echo "No key file found in $CONF_DIR"
         read -rp "Please enter the openweathermap key: " key
 
-        KEY_PATH="/tmp/openweathermap.key"
+        qecho "Writing key to $TMP_KEY_FILE..."
+        echo "$key" > "$TMP_KEY_FILE"
 
-        qecho "Copying key to $INSTALL_PATH"
-        echo "$key" > "$KEY_PATH"
+        qecho "Copying $TMP_KEY_FILE to $NEW_KEY_FILE..."
+        command install -Dm 600 "$TMP_KEY_FILE" "$NEW_KEY_FILE"
+    else
+        qecho "Copying $CONF_KEY_FILE to $NEW_KEY_FILE..."
+        command install -Dm 600 "$CONF_KEY_FILE" "$NEW_KEY_FILE"
     fi
-
-    qecho "Copying key to $INSTALL_PATH"
-    command install -Dm 600 "$KEY_PATH" "$INSTALL_PATH"
 
     qecho "Done copying key"
 }
 
 function cleanup() {
-    qecho "Removing ${temp_files[*]}..."
-    rm -f "${temp_files[@]}"
+    qecho "Removing ${TEMP_FILES[*]}..."
+    rm -f "${TEMP_FILES[@]}"
 }
 
 function uninstall() {
-    qecho "Removing ${new_files[*]}..."
-    rm -f "${new_files[@]}"
+    qecho "Removing ${NEW_FILES[*]}..."
+    rm -f "${NEW_FILES[@]}"
 }
 
 
