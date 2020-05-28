d8856e6ee3968d34ea493f2a59c83a607e9282ac
Mihir Lad <mihirlad55@gmail.com>
2020-05-28 15:45:26 -0400

    Improve code style for wpa-supplicant feature
    Also add separate directory for wpa-supplicant conf.
    See issue #3.

--- optional-features/wpa-supplicant/feature.sh
+++ optional-features/wpa-supplicant/feature.sh
@@ -1,98 +1,119 @@
 #!/usr/bin/bash
 
-
 # Get absolute path to directory of script
-BASE_DIR="$( readlink -f "$(dirname "$0")" )"
+readonly BASE_DIR="$( readlink -f "$(dirname "$0")" )"
 # Get absolute path to root of repo
-LAD_OS_DIR="$( echo "$BASE_DIR" | grep -o ".*/LadOS/" | sed 's/.$//' )"
-INSTALL_CONF_DIR="$LAD_OS_DIR/conf/install"
+readonly LAD_OS_DIR="$( echo "$BASE_DIR" | grep -o ".*/LadOS/" | sed 's/.$//' )"
+readonly CONF_DIR="$LAD_OS_DIR/conf/wpa-supplicant"
+readonly CONF_NETWORK_CONF="$CONF_DIR/network.conf"
+readonly NEW_WPA_SUPPLICANT_DIR="/etc/wpa_supplicant"
+readonly TMP_WPA_SUPPLICANT_CONF="/tmp/wpa_supplicant.conf"
 
 source "$LAD_OS_DIR/common/feature_header.sh"
 
-feature_name="wpa_supplicant"
-feature_desc="Install wpa_supplicant and configuration for network card"
+readonly FEATURE_NAME="WPA Supplicant"
+readonly FEATURE_DESC="Install wpa_supplicant with existing configuration"
+readonly PROVIDES=()
+readonly NEW_FILES=("$NEW_WPA_SUPPLICANT_DIR")
+readonly MODIFIED_FILES=()
+readonly TEMP_FILES=("$TMP_NETWORK_CONF")
+readonly DEPENDS_AUR=()
+readonly DEPENDS_PACMAN=(wpa_supplicant dhcpcd)
+readonly DEPENDS_PIP3=()
 
-provides=()
-new_files=("/etc/wpa_supplicant/")
-modified_files=()
-temp_files=("/tmp/wpa_supplicant.conf")
-
-depends_aur=()
-depends_pacman=(wpa_supplicant dhcpcd)
-depends_pip3=()
 
 
 function check_install() {
     if pacman -Q wpa_supplicant > /dev/null &&
         pacman -Q dhcpcd > /dev/null &&
-        test -f /etc/wpa_supplicant/wpa_supplicant-*.conf; then
-        qecho "$feature_name is installed"
+        test -f "$NEW_WPA_SUPPLICANT_DIR"/wpa_supplicant-*.conf; then
+        qecho "$FEATURE_NAME is installed"
         return 0
     else
-        echo "$feature_name is not installed" >&2
+        echo "$FEATURE_NAME is not installed" >&2
         return 1
     fi
 }
 
 function check_conf() {
-    if [[ -f "$INSTALL_CONF_DIR/network.conf" ]] &&
-        [[ "$(cat "$INSTALL_CONF_DIR/network.conf")" != "" ]]; then
-        qecho "Configuration found at $INSTALL_CONF_DIR/network.conf"
-        return 0
-    else
-        echo "No configuration found at $INSTALL_CONF_DIR/network.conf" >&2
-        return 1
+    local conf
+
+    if [[ -f "$CONF_NETWORK_CONF" ]]; then
+        conf="$(cat "$CONF_NETWORK_CONF")"
+
+        if [[ "$conf" != "" ]]; then
+            qecho "Configuration found at $CONF_NETWORK_CONF"
+            return 0
+        fi
     fi
+
+    echo "No configuration found at $CONF_NETWORK_CONF" >&2
+    return 1
 }
 
 function prepare() {
     ip link
-    echo "Note that the name of this card may change when you boot into the system"
+    echo "Note that the name of this card may change when you boot the system"
     read -rp "Enter name of network card: " card
 
-    echo "ctrl_interface=/run/wpa_supplicant" > /tmp/wpa_supplicant.conf
-    echo "update_config=1" >> /tmp/wpa_supplicant.conf
+    echo "ctrl_interface=/run/wpa_supplicant" > "$TMP_WPA_SUPPLICANT_CONF"
+    echo "update_config=1" >> "$TMP_WPA_SUPPLICANT_CONF"
 
     if check_conf; then
-        cat "$INSTALL_CONF_DIR/network.conf" >> /tmp/wpa_supplicant.conf
+        cat "$CONF_NETWORK_CONF" >> "$TMP_WPA_SUPPLICANT_CONF"
     fi
 
     echo "Opening wpa_supplicant file..."
     read -rp "Press enter to continue..."
     if [[ "$EDITOR" != "" ]]; then
-        "$EDITOR" /tmp/wpa_supplicant.conf
+        "$EDITOR" "$TMP_WPA_SUPPLICANT_CONF"
     else
-        vim /tmp/wpa_supplicant.conf
+        vim "$TMP_WPA_SUPPLICANT_CONF"
     fi
 }
 
 function install() {
-    qecho "Copying /tmp/wpa_supplicant.conf to /etc/wpa_supplciant/wpa_supplicant-${card}.conf..."
-    sudo install -Dm 600 /tmp/wpa_supplicant.conf "/etc/wpa_supplicant/wpa_supplicant-${card}.conf"
+    local name new_wpa_supplicant_conf
+
+    name="wpa_supplicant-${card}.conf"
+    new_wpa_supplicant_conf="$NEW_WPA_SUPPLICANT_DIR/$name"
+    qecho "Copying "$TMP_WPA_SUPPLICANT_CONF" to $new_wpa_supplicant_conf..."
+    sudo install -Dm 600 "$TMP_WPA_SUPPLICANT_CONF" "$new_wpa_supplicant_conf"
 }
 
 function post_install() {
-    qecho "Enabling wpa_supplicant@${card}.service and dhcpcd.service..."
-    sudo systemctl enable "${SYSTEMD_FLAGS[@]}" "wpa_supplicant@${card}.service"
+    local service
+
+    service="wpa_supplicant@${card}.service"
+
+    qecho "Enabling $service and dhcpcd.service..."
+    sudo systemctl enable "${SYSTEMD_FLAGS[@]}" "$service"
     sudo systemctl enable "${SYSTEMD_FLAGS[@]}" dhcpcd.service
-    qecho "Enabled wpa_supplicant@${card}.service and dhcpcd.service"
+    qecho "Enabled $service and dhcpcd.service"
 }
 
 function cleanup() {
-    qecho "Removing ${temp_files[*]}..."
-    rm -rf "${temp_files[@]}"
+    qecho "Removing ${TEMP_FILES[*]}..."
+    rm -rf "${TEMP_FILES[@]}"
 }
 
 function uninstall() {
+    local card service name new_wpa_supplicant_conf
+
     ip link
     read -rp "Enter name of network card: " card
 
-    qecho "Disabling wpa_supplicant@${card}.service and dhcpcd.service..."
-    sudo systemctl disable "${SYSTEMD_FLAGS[@]}" "wpa_supplicant@${card}.service"
+    service="wpa_supplicant@${card}.service"
+    name="wpa_supplicant-${card}.conf"
+    new_wpa_supplicant_conf="$NEW_WPA_SUPPLICANT_DIR/$name"
+
+    qecho "Disabling $service and dhcpcd.service..."
+    sudo systemctl disable "${SYSTEMD_FLAGS[@]}" "$service"
     sudo systemctl disable "${SYSTEMD_FLAGS[@]}" dhcpcd.service
 
-    qecho "Removing /etc/wpa_supplicant/wpa_supplicant-${card}.conf..." 
-    sudo rm -rf "/etc/wpa_supplicant/wpa_supplicant-${card}.conf"
+    qecho "Removing $new_wpa_supplicant_conf..." 
+    sudo rm -rf "$new_wpa_supplicant_conf"
 }
+
 
 source "$LAD_OS_DIR/common/feature_footer.sh"
