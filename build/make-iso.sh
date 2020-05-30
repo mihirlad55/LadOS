40a60396e731afabcba78316eaf04a3fe3f0ee9b
Mihir Lad <mihirlad55@gmail.com>
2020-05-30 19:25:01 -0400

    Add comments to build scripts

--- build/make-iso.sh
+++ build/make-iso.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
  
+# Catch errors
 set -o errtrace
 set -o pipefail
 trap error_trap ERR
 
+# Keep at top so any errors after this can be caught
 function error_trap() {
     error_code="$?"
     error "$(caller): \"$BASH_COMMAND\" returned error code $error_code"
@@ -21,11 +23,13 @@ readonly ISO_PUBLISHER="Mihir Lad <https://mihirlad.com"
 readonly ISO_APPLICATION="LadOS Linux Live CD"
 readonly ISO_OUT_DIR="$BASE_DIR"
 
+# These flags are marked as readonly after being finalized at the bottom
 VERBOSE=
 GIT_FLAGS=("--depth 1")
 V_FLAG=()
 Q_FLAG=("-q")
 
+# Contains screen printing functions
 source "$LAD_OS_DIR/common/message.sh"
 
 
@@ -87,13 +91,29 @@ function vecho() {
     if [[ -n "$VERBOSE" ]]; then echo "$@"; fi
 }
 
+################################################################################
+# Display menu to user with title and options
+#   Globals:
+#     None
+#   Arguments:
+#     title, Title of menu
+#     <option name> <option_function>... Unlimited pairs of option names and
+#     functions to call when corresponding option is selected. Each option name
+#     and function are separate arguments, but must be provided in pairs.
+#   Outputs:
+#     Prompts user with list of options
+#   Returns:
+#     0 if successful
+################################################################################
 function show_menu() {
     local option num_of_options i name name_idx func_idx
 
+    # Print title and shift title out of arguments list
     title="-----$1-----"
     shift
 
     option=0
+    # Option names and thier functions come in pairs
     num_of_options=$(( $# / 2 ))
 
     while true; do
@@ -101,7 +121,9 @@ function show_menu() {
 
         echo "$title"
 
+        # Print every option
         while (( i <= num_of_options )); do
+            # Name of option is the first argument of every pair
             name_idx="$(( i * 2 - 1 ))"
             name="${!name_idx}"
 
@@ -112,6 +134,7 @@ function show_menu() {
 
         read -rp "Option: " option
 
+        # If input is invalid re-print menu
         if echo "$option" | grep -q -P "^[0-9]+$"; then
             if (( option > num_of_options )); then
                 continue
@@ -120,6 +143,7 @@ function show_menu() {
             continue
         fi
 
+        # Call function associated with option (2nd argument in pair)
         func_idx=$(( option * 2 ))
         "${!func_idx}"
     done
@@ -129,6 +153,17 @@ function is_arch_user() {
     pacman -V > /dev/null
 }
 
+################################################################################
+# Make boot directory with squashed recovery mode
+#   Globals:
+#     BASE_DIR
+#   Arguments:
+#     install_dir, Directory to copy recovery directory into
+#   Outputs:
+#     Prints info about progress to user
+#   Returns:
+#     0 if successful
+################################################################################
 function make_recovery() {
     local -r RECOVERY_DIR AIROOTFS_DIR
     local install_dir
@@ -160,6 +195,18 @@ function make_recovery() {
     sudo cp -rft "$install_dir" "$RECOVERY_DIR/work/iso/recovery"
 }
 
+################################################################################
+# Image device with ISO
+#   Globals:
+#     None
+#   Arguments:
+#     iso_path, path to ISO file
+#     dev, path to device (i.e. /dev/sdX)
+#   Outputs:
+#     Prints dd progress
+#   Returns:
+#     0 if successful, error code of dd otherwise
+################################################################################
 function image_dev() {
     local iso_path dev
 
@@ -169,6 +216,20 @@ function image_dev() {
     sudo dd bs=4M if="$iso_path" of="$dev" status=progress oflag=sync
 }
 
+################################################################################
+# Build ttf-ms-win10 AUR package to be included in localrepo. This can be used
+# as an alternative method of installing the win10-fonts feature, so that the
+# package does not have to be compiled during the install. This package requires
+# windows 10 fonts to be present in win10-fonts conf folder.
+#   Globals:
+#     None
+#   Arguments:
+#     PKG_DIR, path to directory to save compiled ttf-ms-win10 package
+#   Outputs:
+#     Prints progress
+#   Returns:
+#     0 if successful
+################################################################################
 function build_win10_fonts() {
     local TMP_DIR FONTS_DIR BUILD_SH PKG_DIR
 
@@ -197,6 +258,19 @@ function build_win10_fonts() {
     fi
 }
 
+################################################################################
+# Build AUR packages from packages.csv to be included in ISO, so these packages
+# don't have to be compiled during the install.
+#   Globals:
+#     LAD_OS_DIR
+#     GIT_FLAGS
+#   Arguments:
+#     PKG_DIR, path to directory to save compiled AUR packages
+#   Outputs:
+#     Prints progress
+#   Returns:
+#     0 if successful
+################################################################################
 function build_aur_packages() {
     local AUR_URL PKG_DIR TMP_DIR
     local aur_packages pkg_name pkg_dir pkg_url
@@ -207,6 +281,7 @@ function build_aur_packages() {
 
     readonly AUR_URL PKG_DIR TMP_DIR
 
+    # Get list of AUR packages from packages.csv
     mapfile -t aur_packages < <(grep "^.*,.*,aur," "$LAD_OS_DIR/packages.csv")
 
     for pkg in "${aur_packages[@]}"; do
@@ -216,13 +291,15 @@ function build_aur_packages() {
 
         msg3 "$pkg_name"
 
+        # Clone if not already done
         if [[ ! -d "$pkg_dir" ]]; then
             git clone "${GIT_FLAGS[@]}" "$pkg_url" "$pkg_dir"
         fi
 
+        # Source PKGBUILD to build beginning of package file name
         (
             source "/var/tmp/$pkg_name/PKGBUILD"
-
+            
             if [[ "$epoch" != "" ]]; then
                 tar_name_prefix="$pkg_name-$epoch:"
             else
@@ -234,8 +311,10 @@ function build_aur_packages() {
                 tar_name_prefix="${tar_name_prefix}${pkgver}"
             fi
 
+            # Look for packages with this prefix
             tar_path="$(find "$PKG_DIR" -iname "$tar_name_prefix*.pkg.tar.xz")"
 
+            # Build package if not found already in localrepo
             if [[ "$tar_path" != "" ]]; then
                 msg4 "Building $pkg_name..."
 
@@ -251,6 +330,22 @@ function build_aur_packages() {
     done
 }
 
+################################################################################
+# Copy pacman packages from packages.csv to $PKG_DIR. Copy any packages found in
+# host pacman cache to $PKG_DIR. Download any packages not found in cache and
+# copy them to $PKG_DIR. Also include any packages.x86_64 from archiso root
+# directory if found. Add localrepo include directive to specified pacman.conf
+#   Globals:
+#     LAD_OS_DIR
+#   Arguments:
+#     PKG_DIR, path to directory to save compiled AUR packages
+#     ARCH_ISO_DIR, path to archiso root directory
+#     PACMAN_CONF, path to pacman conf to add localrepo include directive
+#   Outputs:
+#     Prints progress
+#   Returns:
+#     0 if successful
+################################################################################
 function copy_pacman_packages() {
     local PKG_DIR ARCH_ISO_DIR PACMAN_CONF TMP_DB_DIR pacman_packages
     local ARCH_ISO_PACKAGE_LIST
@@ -262,14 +357,17 @@ function copy_pacman_packages() {
     TMP_DB_DIR="/tmp"
     ARCH_ISO_PACKAGE_LIST="$ARCH_ISO_DIR/packages.x86_64"
     
+    # Get names of pacman packages from packages.csv
     mapfile -t pacman_packages < <(cat "$LAD_OS_DIR/packages.csv" | \
         grep "^.*,.*,system," | \
         cut -d ',' -f1)
 
     readonly PKG_DIR ARCH_ISO_DIR PACMAN_CONF TMP_DB_DIR ARCH_ISO_PACKAGE_LIST
 
+    # Sync pacman and upgrade system
     sudo pacman -Syu --noconfirm
 
+    # Include packages.x86_64 if found
     if [[ -f "$ARCH_ISO_PACKAGE_LIST" ]]; then
         msg3 "Found package list at $ARCH_ISO_PACKAGE_LIST..."
         mapfile -t arch_iso_packages < "$ARCH_ISO_PACKAGE_LIST"
@@ -281,13 +379,17 @@ function copy_pacman_packages() {
     for pkg in ${pacman_packages[@]}; do
         msg3 "$pkg"
 
+        # Get list of targets to package
         mapfile -t target_paths < <(pacman -Spdd $pkg)
 
+        # If $pkg is a group i.e. base-devel, there will be multiple paths
         for target_path in "${target_paths[@]}"; do 
+            # If target begins with file://, package can be found in cache
             if [[ "$target_path" =~ file://** ]]; then
                 target="${target_path##file://**/}"
                 target_path="${target_path#file://}"
 
+                # If package is not found in localrepo, copy package from cache
                 if [[ ! -f "$PKG_DIR/$target" ]]; then
                     msg4 "Copying $target from cache to the archiso"
                     vecho "Copying from $target_path to $PKG_DIR..."
@@ -296,21 +398,25 @@ function copy_pacman_packages() {
                     msg4 "$target is already in localrepo"
                 fi
             else
+                # Package is not in cache
                 target="${target_path##https://**/}"
                 msg4 "$target not found in host pacman cache"
             fi
         done
     done
 
-
+    # Create a temporary pacman database
     sudo pacman -Sy --noconfirm --dbpath "$TMP_DB_DIR"
 
+    # Use temporary pacman database to download missing packages in $PKG_DIR
     msg3 "Downloading missing packages..."
     sudo pacman -S ${pacman_packages[@]} \
         -w --cachedir "$PKG_DIR" \
         --dbpath "$TMP_DB_DIR" \
         --noconfirm --needed
 
+    # Add localrepo include directive in pacman.conf, so archiso build can use
+    # the localrepo package cache
 	if ! grep -q "$PACMAN_CONF" -e "LadOS"; then
         msg3 "Adding localrepo to pacman.conf"
 	    sudo sed -i "$PACMAN_CONF" \
@@ -318,6 +424,18 @@ function copy_pacman_packages() {
     fi
 }
 
+################################################################################
+# Add kernel commandline parameter to all systemd-boot entries to increase
+# scrollback buffer.
+#   Globals:
+#     None
+#   Arguments:
+#     ENTRIES_DIR, path to systemd-boot boot entries directory
+#   Outputs:
+#     Prints progress
+#   Returns:
+#     0 if successful
+################################################################################
 function increase_tty_scrollback() {
     local ENTRIES_DIR OPTION
 
@@ -332,6 +450,20 @@ function increase_tty_scrollback() {
     fi
 }
 
+################################################################################
+# Create a local pacman repo with a package cache which can be used during the
+# LadOS install, so packages don't have to be compiled or downloaded during the
+# install
+#   Globals:
+#     Q_FLAG
+#   Arguments:
+#     AIROOTFS_DIR, path to airootfs directory in archiso
+#     PACMAN_CONF, path to pacman conf in archiso
+#   Outputs:
+#     Prints progress
+#   Returns:
+#     0 if successful
+################################################################################
 function create_localrepo() {
     local AIROOTFS_DIR PACMAN_CONF LOCAL_REPO_DIR PKG_DIR
 
@@ -342,6 +474,7 @@ function create_localrepo() {
 
     readonly AIROOTFS_DIR PACMAN_CONF LOCAL_REPO_DIR PKG_DIR
 
+    # Install pacman-contrib to use paccache
     sudo pacman -S pacman-contrib --needed --noconfirm
 
     sudo mkdir -p "$PKG_DIR"
@@ -366,6 +499,25 @@ function create_localrepo() {
         localrepo.db.tar.gz pkg/*) || true
 }
 
+################################################################################
+# The main function for the build from scratch method of building the archiso.
+#   Globals:
+#     BASE_DIR
+#     LAD_OS_DIR
+#     SB_KEY_PATH
+#     SB_CRT_PATH
+#     ISO_NAME
+#     ISO_PUBLISHER
+#     ISO_APPLICATION
+#     Q_FLAG
+#     DEV
+#   Arguments:
+#     None
+#   Outputs:
+#     Prints progress
+#   Returns:
+#     0 if successful, 1 otherwise
+################################################################################
 function build_from_scratch() {
     local ARCH_ISO_DIR AIROOTFS_DIR RECOVERY_DIR BOOT_ENTRIES_DIR PACMAN_CONF
     local sb_key_path sb_crt_path res out dev
@@ -458,6 +610,17 @@ function build_from_scratch() {
     fi
 }
 
+################################################################################
+# Download the latest archiso
+#   Globals:
+#     None
+#   Arguments:
+#     ARCH_ISO, path to download archiso to
+#   Outputs:
+#     Progress info
+#   Returns:
+#     0 if successful, error code of curl otherwise
+################################################################################
 function download_iso() {
     local ARCH_ISO
     local top_mirror url_root iso_name url_iso
@@ -465,25 +628,40 @@ function download_iso() {
     ARCH_ISO="$1"
     readonly ARCH_ISO
 
+    # Get first uncommented mirror from /etc/pacman.d/mirrorlist
     top_mirror="$(cat /etc/pacman.d/mirrorlist \
         | grep "^Server = " \
         | head -n1 \
         | cut -d'=' -f2 \
         | awk '{$1=$1; print}')"
 
+    # Get beginning of url
     url_root="$(echo "$top_mirror" \
         | sed -e "s;\$repo/os/\$arch;iso/latest;")"
 
+    # Get name of iso file by grepping html
     iso_name="$(curl -Ls "$url_root" \
         | grep -o -P -e "archlinux-[0-9]{4}\.[0-9]{2}\.[0-9]{2}-x86_64\.iso" \
         | head -n1)"
 
+    # Construct full URL to iso
     url_iso="$url_root/$iso_name"
 
     msg "Downloading archiso..."
     curl "$url_iso" --output "$ARCH_ISO"
 }
 
+################################################################################
+# Download latest archiso and remaster downloaded ISO
+#   Globals:
+#     None
+#   Arguments:
+#     None
+#   Outputs:
+#     Prints progress
+#   Returns:
+#     0 if successful, 1 otherwise
+################################################################################
 function download_and_remaster() {
     local ARCH_ISO
     ARCH_ISO="/tmp/archiso.iso"
@@ -494,6 +672,18 @@ function download_and_remaster() {
     remaster "$ARCH_ISO"
 }
 
+################################################################################
+# Prompt user for path to existing archiso to remaster and begin remastering
+# specified ISO
+#   Globals:
+#     None
+#   Arguments:
+#     None
+#   Outputs:
+#     Prints progress
+#   Returns:
+#     0 if successful, 1 otherwise
+################################################################################
 function use_existing_and_remaster() {
     local arch_iso
 
@@ -502,6 +692,18 @@ function use_existing_and_remaster() {
     remaster "$arch_iso"
 }
 
+################################################################################
+# Prompt user for device path to image with built LadOS ISO
+#   Globals:
+#     BASE_DIR
+#     DEV
+#   Arguments:
+#     None
+#   Outputs:
+#     Prints progress
+#   Returns:
+#     0 if successful, 1 otherwise
+################################################################################
 function existing_image_to_usb() {
     iso_path="$(find "$BASE_DIR" -type f -name 'LadOS*.iso' -print -quit)"
 
@@ -515,6 +717,25 @@ function existing_image_to_usb() {
     fi
 }
 
+################################################################################
+# The main function for the remaster method of building the archiso.
+#   Globals:
+#     BASE_DIR
+#     LAD_OS_DIR
+#     SB_KEY_PATH
+#     SB_CRT_PATH
+#     ISO_NAME
+#     ISO_PUBLISHER
+#     ISO_APPLICATION
+#     Q_FLAG
+#     DEV
+#   Arguments:
+#     ARCH_ISO, path to archiso file
+#   Outputs:
+#     Prints progress
+#   Returns:
+#     0 if successful, 1 otherwise
+################################################################################
 function remaster() {
     local ARCH_ISO ARCH_ISO_DIR EFI_BOOT_DIR CUSTOM_ISO_DIR AIROOTFS_SFS
     local SHA512_AIROOTFS_SFS SQUASHFS_ROOT_DIR BOOT_ENTRIES_DIR RECOVERY_DIR
@@ -560,10 +781,12 @@ function remaster() {
         sudo umount "$ARCH_ISO_DIR"
     fi
     
+    # Mount archiso
     msg "Mounting archiso to $ARCH_ISO_DIR..."
     sudo mkdir -p "$ARCH_ISO_DIR"
     sudo mount -t iso9660 -o loop "$ARCH_ISO" "$ARCH_ISO_DIR"
 
+    # Copy contents of archiso to tmp directory
     msg  "Copying archiso to $CUSTOM_ISO_DIR..."
     sudo mkdir -p "$CUSTOM_ISO_DIR"
     sudo cp -a $ARCH_ISO_DIR/* "$CUSTOM_ISO_DIR"
@@ -610,6 +833,7 @@ function remaster() {
         sb_crt_path="$SB_CRT_PATH"
     fi
 
+    # Use sbsign to sign EFI boot binaries for secure boot
     if [[ -n "$sb_key_path" ]] && [[ -n "$sb_crt_path" ]]; then
         msg2 "Signing EFI and vmlinuz binaries in $CUSTOM_ISO_DIR..."
         sudo find "$CUSTOM_ISO_DIR" \( -iname '*.efi' -o -iname 'vmlinuz*' \) \
@@ -629,16 +853,19 @@ function remaster() {
         sudo umount "$EFI_BOOT_DIR"
     fi
 
+    # Get label from systemd-boot entry
     msg "Creating new iso..."
     label="$(cat "$ARCH_ISO_BOOT_CONF" \
         | tail -n1 \
         | grep -o "archisolabel=.*$" \
         | cut -d'=' -f2)"
 
+    # Name of remastered ISO
     out="$ISO_OUT_DIR/$(date +$ISO_NAME-%Y.%m.%d-x86_64.iso)"
 
     sudo rm -f "$out"
 
+    # Create remastered ISO
     sudo xorriso -as mkisofs \
         -full-iso9660-filenames \
         -volid "$label" \
@@ -652,6 +879,7 @@ function remaster() {
         -output "$out" \
         "$CUSTOM_ISO_DIR"
 
+    # Image device with remastered ISO
     if [[ -n "$DEV" ]]; then
         image_dev "$out" "$DEV"
     elif iprompt "Would you like to write this image to a device? "; then
@@ -664,6 +892,17 @@ function remaster() {
     exit 0
 }
 
+################################################################################
+# Display remaster menu
+#   Globals:
+#     None
+#   Arguments:
+#     None
+#   Outputs:
+#     Prints menu
+#   Returns:
+#     0 if successful, 1 otherwise
+################################################################################
 function remaster_iso() {
     show_menu "Remaster ISO" \
         "Download ISO"      "download_and_remaster" \
@@ -671,6 +910,17 @@ function remaster_iso() {
         "Go Back"           "return 0"
 }
 
+################################################################################
+# Display main menu
+#   Globals:
+#     None
+#   Arguments:
+#     None
+#   Outputs:
+#     Prints menu
+#   Returns:
+#     0 if successful, 1 otherwise
+################################################################################
 function interactive() {
     if is_arch_user; then
         show_menu "Make ISO" \
@@ -694,6 +944,7 @@ function interactive() {
 
 
 
+# Parse script arguments and set main mode
 case "$1" in
     interactive | build | remaster)
         CMD="$1"
@@ -715,6 +966,7 @@ case "$1" in
         exit 1
 esac
 
+# Parse commandline options and set global flags using given arguments
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
         -a | --auto-download-iso)
@@ -781,6 +1033,7 @@ readonly CMD AUTO_DOWNLOAD_ISO SB_CRT_PATH DEV SB_KEY_PATH CREATE_LOCAL_REPO
 readonly BUILD_TTF_MS_WIN_10 VERBOSITY Q_FLAG V_FLAG GIT_FLAGS
 
 
+# Start mode
 case "$CMD" in
     interactive)
         interactive
--- build/misc/build-ttf-ms-win10.sh
+++ build/misc/build-ttf-ms-win10.sh
@@ -12,11 +12,12 @@ readonly BUILD_SH="$FEATURE_DIR/feature-build.sh"
 readonly PKG_DIR="$1"
 
 
-
+# Try to load fonts
 "$BUILD_SH" check_conf && "$BUILD_SH" load_conf
 
 "$BUILD_SH" prepare
 
+# Copy compiled package to PKG_DIR
 echo "Copying $PKG_BUILD_DIR*.pkg.tar.xz to $PKG_DIR..."
 find "$PKG_BUILD_DIR" -name '*.pkg.tar.xz' -exec cp -f {} "$PKG_DIR" \;
 
