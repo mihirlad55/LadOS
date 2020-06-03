2bee2f2459ae68a7cf22445de852aefbd49dfd21
Mihir Lad <mihirlad55@gmail.com>
2020-06-03 19:54:24 -0400

    Fix dracut-install-default.sh in dracut feature
    Using uname -r doesn't work for chroots since it returns the kernel version of
    the host system (i.e. the booted archiso) which may be different from the
    chrooted system.
    
    Using ls fixes this issue since it shows the actual installed kernel versions.

--- required-features/1-dracut/dracut-install-default.sh
+++ required-features/1-dracut/dracut-install-default.sh
@@ -5,7 +5,10 @@ cmdline="$(cat /etc/cmdline.d/* | tr '\n' ' ')"
 args=('--force' '--uefi' '--hostonly' '--no-hostonly-cmdline' \
     '--kernel-cmdline' "$cmdline")
 
-kver="$(uname -r)"
+# Use ls to get installed version. When chrooted from an archiso, uname -r
+# returns kernel version of archiso instead of the new system
+
+kver="$(ls /usr/lib/modules | head -n1)"
 read -r pkgbase < "/usr/lib/modules/$kver/pkgbase"
 
 # Backup image
