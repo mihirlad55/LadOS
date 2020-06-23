81745e0f1c45e40f673ca939e37feb48ba1aac8d
Mihir Lad <mihirlad55@gmail.com>
2020-06-23 17:25:51 -0400

    Add make-virsh.sh script to build
    This script sets up and installs a QEMU virtual machine to test the ISO.

---
+++ build/make-virsh.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+readonly BASE_DIR="$( readlink -f "$(dirname "$0")" )"
+
+virsh destroy --domain ArchLinux
+virsh undefine --nvram --domain ArchLinux
+
+mkdir -p "$HOME/shared"
+
+virt-install \
+    --connect qemu:///session \
+    --name ArchLinux \
+    --description "Arch Linux" \
+    --os-type="Linux" \
+    --os-variant="archlinux" \
+    --ram=1024 \
+    --vcpus=2 \
+    --cpu host-model-only \
+    --disk path="$HOME"/virtual-machines/archlinux.qcow2,bus=virtio,size=20,format=qcow2 \
+    --check path_in_use=off \
+    --cdrom "$BASE_DIR"/LadOS*.iso \
+    --boot uefi \
+    --graphics spice \
+    --filesystem "$HOME/shared,shared"
+
+# To mount filesystem on guest
+# mount -t 9p -o trans=virtio shared /shared -oversion=9p2000.L
