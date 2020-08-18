2b3b0d001431da59ee19ffd82ded57113b92ae7e
Mihir Lad <mihirlad55@gmail.com>
2020-08-17 23:48:43 -0400

    Merge archiso v46 releng into recovery config

--- build/recovery/airootfs/root/customize_airootfs.sh
+++ build/recovery/airootfs/root/customize_airootfs.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 set -e -u
 
+echo 'Warning: customize_airootfs.sh is deprecated! Support for it will be removed in a future archiso version.'
+
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
-
-usermod -s /usr/bin/zsh root
-cp -aT /etc/skel/ /root/
-chmod 700 /root
 
 # Set root password
 echo "Enter password to set for recovery root: "
 passwd
 
-sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
