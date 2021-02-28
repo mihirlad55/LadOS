b0fc4cb902d66b299ff1f592de9611e5e1936d07
Mihir Lad <mihirlad55@gmail.com>
2021-02-28 00:47:17 -0500

    Create fallback dracut image instead of backup

--- required-features/01-dracut/dracut-install-default.sh
+++ required-features/01-dracut/dracut-install-default.sh
@@ -5,13 +5,17 @@ cmdline="$(cat /etc/cmdline.d/* | tr '\n' ' ')"
 args=('--force' '--uefi' '--hostonly' '--no-hostonly-cmdline' \
     '--kernel-cmdline' "$cmdline")
 
+args_fallback=('--force' '--uefi' '--no-hostonly' '--no-hostonly-cmdline' \
+  '--kernel-cmdline' "$cmdline")
+
 # Use ls to get installed version. When chrooted from an archiso, uname -r
 # returns kernel version of archiso instead of the new system
 
 kver="$(ls /usr/lib/modules | head -n1)"
 read -r pkgbase < "/usr/lib/modules/$kver/pkgbase"
 
-# Backup image
-cp -f "/boot/${pkgbase}.efi" "/boot/${pkgbase}-fallback.efi"
+# Fallback image
+dracut "${args_fallback[@]}" "/boot/${pkgbase}-fallback.efi" \
+  --kver "$kver"
 
 dracut "${args[@]}" "/boot/${pkgbase}.efi" --kver "$kver"
--- required-features/01-dracut/dracut-install.sh
+++ required-features/01-dracut/dracut-install.sh
@@ -5,14 +5,18 @@ cmdline="$(cat /etc/cmdline.d/* | tr '\n' ' ')"
 args=('--force' '--uefi' '--hostonly' '--no-hostonly-cmdline' \
     '--kernel-cmdline' "$cmdline")
 
+args_fallback=('--force' '--uefi' '--no-hostonly' '--no-hostonly-cmdline' \
+  '--kernel-cmdline' "$cmdline")
+
 while read -r line; do
     if [[ "$line" == 'usr/lib/modules/'+([^/])'/pkgbase' ]]; then
 		read -r pkgbase < "/${line}"
 		kver="${line#'usr/lib/modules/'}"
 		kver="${kver%'/pkgbase'}"
 
-        # Backup image
-        cp -f "/boot/${pkgbase}.efi" "/boot/${pkgbase}-fallback.efi"
+        # Fallback image
+		dracut "${args_fallback[@]}" "/boot/${pkgbase}-fallback.efi" \
+          --kver "$kver"
 
 		dracut "${args[@]}" "/boot/${pkgbase}.efi" --kver "$kver"
 	fi
